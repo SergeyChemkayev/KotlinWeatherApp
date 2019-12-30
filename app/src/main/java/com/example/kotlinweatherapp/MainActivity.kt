79c195b153a255e69f67b09281604b74883897d3
Sergey Chemkayev <chemkayev.sergey@gmail.com>
2019-12-30 15:19:45 +0300

    optimising imports

--- app/src/main/java/com/example/kotlinweatherapp/MainActivity.kt
+++ app/src/main/java/com/example/kotlinweatherapp/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.example.kotlinweatherapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
