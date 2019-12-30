5a353942c80d0308a9275d2b1cf36d0c7f8c8f0e
Sergey Chemkayev <chemkayev.sergey@gmail.com>
2019-12-30 14:24:53 +0300

    Initial commit

---
+++ app/src/main/java/com/example/kotlinweatherapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.kotlinweatherapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val name = "Sergey"
+        message.text = "Hello ${name}!"
+    }
+}
